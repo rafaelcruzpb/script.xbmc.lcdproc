812e878f8673661b927c571c1aa2490b3c3512d8
Daniel Scheller <d.scheller@gmx.net>
2018-06-24 19:58:49 +0200

    thread testing

--- resources/lib/kodibroker.py
+++ resources/lib/kodibroker.py
@@ -25,6 +25,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import json
+import time
+
 from .common import *
 from .thread import *
 
@@ -37,8 +40,68 @@ class KodiBroker(Thread, xbmc.Monitor):
         Thread.__init__(self)
         xbmc.Monitor.__init__(self)
 
+        # prepare infobool strings to save some cpu ticks later
+        self._cKaiToastActive = "Window.IsActive(" + str(WINDOW_IDS.WINDOW_DIALOG_KAI_TOAST) + ")"
+        self._cVolumeBarActive = "Window.IsActive(" + str(WINDOW_IDS.WINDOW_DIALOG_VOLUME_BAR) + ")"
+
         # interval to use for async polling
         self._asyncinterval = 0.2
+
+        self._useExecJRPC = True
+        self._jrpcdebug = False
+        self._counter = 0
+
+    def jsonrpc_get(self, method, params):
+        jsondata = {
+            "jsonrpc": "2.0",
+            "method": method,
+            "id": method}
+
+        if params:
+            jsondata["params"] = params
+
+        try:
+            if self._jrpcdebug: log(LOGDEBUG, "==== JSONRPC debug begin ====")
+
+            rpccmd = json.dumps(jsondata)
+            if self._jrpcdebug: log(LOGDEBUG, "JSONRPC out: " + rpccmd)
+
+            rpcreply = xbmc.executeJSONRPC(rpccmd)
+
+            if self._jrpcdebug:
+                log(LOGDEBUG, "JSONRPC in: " + rpcreply)
+                log(LOGNOTICE, "==== JSONRPC debug end ====")
+
+            rpcdata = json.loads(rpcreply)
+
+            if rpcdata["id"] == method and rpcdata.has_key("result"):
+                return rpcdata["result"]
+        except Exception as e:
+            log(LOGERROR, "Caught JSONRPC exception: %s" % (str(e)))
+
+        return False
+
+    def getlabels(self, labels):
+        if self._useExecJRPC:
+            return self.jsonrpc_get("XBMC.GetInfoLabels",
+                {"labels": labels})
+
+        ret = {}
+        for label in labels:
+            ret[label] = xbmc.getInfoLabel(label)
+
+        return ret
+
+    def getbools(self, bools):
+        if self._useExecJRPC:
+            return self.jsonrpc_get("XBMC.GetInfoBooleans",
+                {"booleans": bools})
+
+        ret = {}
+        for bool in bools:
+            ret[bool] = xbmc.getCondVisibility(bool)
+
+        return ret
 
     ########
     # run(): background thread used for async state inquiry for all things
@@ -49,7 +112,32 @@ class KodiBroker(Thread, xbmc.Monitor):
         log(LOGDEBUG, "Started thread '%s'" % (self.getthreadid()))
 
         # keep on working until being told otherwise
-        while not self.cancelled():
-            self.waitForAbort(self._asyncinterval)
+        while not self.cancelled() and not self.waitForAbort(self._asyncinterval):
+            self.syncStatePeriodic()
 
         log(LOGDEBUG, "Finished thread '%s'" % (self.getthreadid()))
+
+    def syncStatePeriodic(self):
+        # init local vars
+        oldwindow = ""
+        oldcontrol = ""
+
+        # list of InfoLabels to load
+        labels = self.getlabels([
+            "MusicPlayer.Channels",
+            "Player.Duration",
+            "Player.Time",
+            "System.CurrentWindow",
+            "System.CurrentControl",
+            "System.ScreenHeight",
+            "System.Time(hh:mm:ss)",
+            "VideoPlayer.AudioChannels",
+        ])
+
+        # list of InfoBools (aka. CondVisibility)
+        bools = self.getbools([
+            "PVR.IsRecording",
+            "Player.Passthrough",
+            self._cKaiToastActive,
+            self._cVolumeBarActive
+        ])
